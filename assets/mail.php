bb4f261a06c9057d432e1d7cf0a42d887cd1a103
Fynn Menk <mail@fynnmenk.de>
2025-01-11 19:14:42 +0100

    Add Fonts, Add Img 404, Add Mail functionality, add ddev

--- assets/mail.php
+++ assets/mail.php
@@ -1,111 +1,48 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    // Formularfelder abrufen und bereinigen
+    $vorname = strip_tags(trim($_POST["vorname"]));
+    $nachname = strip_tags(trim($_POST["nachname"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
 
-
-    // Only process POST reqeusts.
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        // Get the form fields and remove MORALspace.
-
-        $name = strip_tags(trim($_POST["name"]));
-
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-
-
-        $message = trim($_POST["message"]);
-
-
-
-        // Check that data was sent to the mailer.
-
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-            // Set a 400 (bad request) response code and exit.
-
-            http_response_code(400);
-
-            echo "Please complete the form and try again.";
-
-            exit;
-
-        }
-
-
-
-        // Set the recipient email address.
-
-        // FIXME: Update this to your desired email address.
-
-        $recipient = "basictheme400@gmail.com";
-
-
-
-        // Set the email subject.
-
-        $sender = "New contact from $name";
-
-
-
-        //Email Header
-
-        $head = " /// STTHEMES \\\ ";
-
-
-
-        // Build the email content.
-
-        $email_content = "$head\n\n\n";
-
-        $email_content .= "Name: $name\n";
-
-        $email_content .= "Email: $email\n\n";
-
-        $email_content .= "Message:\n$message\n";
-
-
-
-        // Build the email headers.
-
-        $email_headers = "From: $name <$email>";
-
-
-
-        // Send the email.
-
-        if (mail($recipient, $sender, $email_content, $email_headers)) {
-
-            // Set a 200 (okay) response code.
-
-            http_response_code(200);
-
-            echo "Thank You! Your message has been sent.";
-
-        } else {
-
-            // Set a 500 (internal server error) response code.
-
-            http_response_code(500);
-
-            echo "Oops! Something went wrong and we couldn't send your message.";
-
-        }
-
-
-
-    } else {
-
-        // Not a POST request, set a 403 (forbidden) response code.
-
-        http_response_code(403);
-
-        echo "There was a problem with your submission, please try again.";
-
+    // Überprüfen, ob die Daten an den Mailer gesendet wurden
+    if (empty($vorname) or empty($nachname) or empty($subject) or empty($message) or !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Setze eine 400 (schlechte Anfrage) Antwort und beende die Ausführung
+        http_response_code(400);
+        echo "Bitte füllen Sie das Formular vollständig aus und versuchen Sie es erneut.";
+        exit;
     }
 
+    // Empfänger-E-Mail-Adresse
+    $recipient = "fynn.menk@gmail.com";
+    $subject = "Neue Kontaktanfrage: $subject";
 
+    // E-Mail-Inhalt
+    $email_content = "Vorname: $vorname\n";
+    $email_content .= "Nachname: $nachname\n";
+    $email_content .= "E-Mail: $email\n\n";
+    $email_content .= "Betreff: $subject\n\n";
+    $email_content .= "Nachricht:\n$message\n";
 
-?>
+    // E-Mail-Header
+    $email_headers = "From: $vorname $nachname <$email>";
 
+    // Versuche die E-Mail zu senden
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Setze eine 200 (Erfolg) Antwort
+        http_response_code(200);
+        echo "Danke! Ihre Nachricht wurde gesendet.";
+    } else {
+        // Setze eine 500 (interner Serverfehler) Antwort
+        http_response_code(500);
+        echo "Oops! Etwas ist schief gelaufen und wir konnten Ihre Nachricht nicht senden.";
+    }
+
+} else {
+    // Nicht eine POST-Anfrage, setze eine 403 (verboten) Antwort
+    http_response_code(403);
+    echo "Es gab ein Problem mit Ihrer Einsendung, bitte versuchen Sie es erneut.";
+}
