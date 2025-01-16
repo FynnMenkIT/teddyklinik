4ac585b06f1719ee37a9de8422980d81654570ca
Fynn Menk <mail@fynnmenk.de>
2025-01-16 13:00:34 +0100

    move data bbecause of safety

--- public/assets/mail.php
+++ public/assets/mail.php
@@ -3,10 +3,10 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use Dotenv\Dotenv;
 
-require '../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 // Laden der Umgebungsvariablen
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../config/');
 $dotenv->load();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
